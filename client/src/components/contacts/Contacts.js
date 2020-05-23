958793fda7dbf189d1c8205a1b4a0c91b8b1bc9a
leey611 <leey611@newschool.edu>
2020-05-23 11:22:12 -0400

    finish course

---
+++ client/src/components/contacts/Contacts.js
@@ -0,0 +1,42 @@
+import React, { Fragment, useContext, useEffect } from 'react';
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
+import ContactContext from '../../context/contact/contactContext';
+import Spinner from '../layout/Spinner'
+import ContactItem from './ContactItem'
+
+const Contacts = () => {
+    const contactContext = useContext(ContactContext);
+    const { contacts, filtered, getContacts, loading } = contactContext;
+
+    useEffect(() => {
+        getContacts();
+        // eslint-disable-next-line
+    }, [])
+
+    if (contacts !== null && contacts.length === 0 && !loading) {
+        return <h4>Add your contacts</h4>
+    }
+
+    return (
+        <Fragment>
+            {contacts !== null && !loading ? (
+                <TransitionGroup>
+                    {filtered !== null ?
+                        filtered.map(contact => (
+                        <CSSTransition key={contact._id} timeout={500} className="item">
+                        <ContactItem contact={contact}></ContactItem>
+                    </CSSTransition>
+                ))
+                : contacts.map(contact => (
+                    <CSSTransition key={contact._id} timeout={500} classNames="item">
+                        <ContactItem contact={contact}></ContactItem>
+                    </CSSTransition>
+                ))}
+            </TransitionGroup>) : <Spinner />}
+            
+            
+        </Fragment>
+    )
+}
+
+export default Contacts
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,60 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator');
+
+
+const User = require('../models/User');
 
 // @route   POST api/users
 // @desc    Register a user
 // @access  Public
-router.post('/', (req, res) => {
-    res.send('Register a user');
+router.post('/', [
+    check('name', 'Please add name').not().isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })
+], async (req, res) => {
+    const errors = validationResult(req);  
+    if(!errors.isEmpty()) { // if "not errors" "is empty", so means there are errors
+        return res.status(400).json({ errors: errors.array() });
+    }
+    
+    const { name, email, password } = req.body;
+
+    try {
+        let user = await User.findOne( { email }) //check if email has been registered
+        if (user) {
+            return res.status(400).json({ msg: 'User already exists' });
+        }
+
+        user = new User({
+            name: name,
+            email: email,
+            password: password
+        });
+
+        const salt = await bcrypt.genSalt(10); //use bcrypt to encrypt the password and define how secure the salt is
+        user.password = await bcrypt.hash(password, salt)  // takes two params (plain text password & salt) and return a hash version password, then re-assign
+        await user.save();
+        
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+
+        jwt.sign(payload, config.get('jwtSecret'), {
+            expiresIn: 360000
+        }, (err, token) => {
+            if(err) throw err;
+            res.json({ token });
+        }) //sign to generate a token
+    } catch (err) {
+        console.error(err.message)
+        res.status(500).send('server error')
+    }
 });
 
 module.exports = router;
